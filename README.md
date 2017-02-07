9dfb6726803c662704bb4d762756ff9e761f5d0d
Bonen <koen.bonenkamp@gmail.com>
2017-02-07 13:06:20 +0100

    vagrant with java/docker setup

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # idintt
 Dutch Blockchain Hackathon contribution team iDINtt
+
+## Instructions
+Download git and vagrant
+
+(go to your working directory of choice)
+$ git clone https://github.com/notarynodes/idintt.git
+$ cd idintt
+$ vagrant up --provider virtualbox
+
+Now a vagrant box with java, maven, docker & docker-compose is created.
+SSH into the box to run the fabric dockers provided by yeasy.
+
+$ vagrant ssh
+$ sudo chmod u+x /vagrant/fabric-docker/init.sh
+$ sudo /vagrant/fabric-docker/init.sh
+
+## Directory structure
+
+### fabric docker
+Fabric 1.0 docker compose files to launch the fabric CA, peer and orderer (by yeasy)
+
+### fabric-sdk-java-master
+Fabric 1.0 java library developed Hyperledger fabric team
+
+### idin-library-java1.041
+Java library developed by iDIN
+
+### idintt-fabric-module
+The Hackathon contribution, development starts after the opening on Friday February 10th!!
--- fabric-docker/init.sh
+++ fabric-docker/init.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+####################################
+#
+# Pull images and docker-compose to run ca, orderer and peer
+#
+####################################
+
+curl -o docker-compose.yml https://raw.githubusercontent.com/yeasy/docker-compose-files/master/hyperledger/1.0/docker-compose.yml
+curl -o peer.yml https://raw.githubusercontent.com/yeasy/docker-compose-files/master/hyperledger/1.0/peer.yml
 
 docker pull yeasy/hyperledger-fabric-base:latest \
   && docker pull yeasy/hyperledger-fabric-peer:latest \
@@ -9,9 +17,7 @@ docker pull yeasy/hyperledger-fabric-base:latest \
   && docker tag yeasy/hyperledger-fabric-orderer hyperledger/fabric-orderer \
   && docker tag yeasy/hyperledger-fabric-ca hyperledger/fabric-ca \
   && docker tag yeasy/hyperledger-fabric-base hyperledger/fabric-baseimage \
-  && docker tag yeasy/hyperledger-fabric-base hyperledger/fabric-ccenv:x86_64-1.0.0-preview \
-  && docker tag hyperledger/fabric-ccenv:x86_64-1.0.0-preview hyperledger/fabric-ccenv:x86_64-1.0.0-snapshot-preview
-
-docker build ./orderer-fix/ -t hyperledger/fabric-orderer-fix
+  && docker tag yeasy/hyperledger-fabric-base hyperledger/fabric-ccenv:x86_64-1.0.0-preview
 
 docker-compose up
+# docker-compose up -d
